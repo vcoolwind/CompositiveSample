d16b81b0578f1df6ff48327748d36ed07d14befe
vcoolwind <vcoolwind@163.com>
2016-11-08 10:06:32 +0800

    完善相机应用

--- app/src/main/java/vcoolwind/com/compositivesample/ui/CameraActivity.java
+++ app/src/main/java/vcoolwind/com/compositivesample/ui/CameraActivity.java
@@ -1,16 +1,28 @@
 package vcoolwind.com.compositivesample.ui;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.service.carrier.CarrierService;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import vcoolwind.com.compositivesample.R;
 
@@ -20,6 +32,7 @@ import vcoolwind.com.compositivesample.R;
 
 public class CameraActivity extends AppCompatActivity {
     ImageView imageView_camera = null;
+    String mCurrentTmpPicFile = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,6 +54,8 @@ public class CameraActivity extends AppCompatActivity {
                 dispatchTakePictureIntent();
                 return true;
             case R.id.save_photo:
+                verifyStoragePermissions(this);
+                doTakePictureAndSave();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -59,6 +74,16 @@ public class CameraActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(this, "照相失败！", Toast.LENGTH_SHORT).show();
                 }
+            case REQUEST_IMAGE_CAPTURE_SAVE:
+                if (resultCode == RESULT_OK) {
+                    Log.i(getClass().getSimpleName(), "通知：" + mCurrentTmpPicFile);
+                    galleryAddPic(mCurrentTmpPicFile);
+
+                } else {
+                    Toast.makeText(this, "照相失败！", Toast.LENGTH_SHORT).show();
+                }
+
+
             default:
                 super.onActivityResult(requestCode, resultCode, data);
         }
@@ -76,5 +101,87 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
+    static final int REQUEST_IMAGE_CAPTURE_SAVE = 2;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+
+        Log.i(getClass().getSimpleName(), storageDir.getAbsolutePath());
+
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentTmpPicFile = image.getAbsolutePath();
+
+        Log.i(getClass().getSimpleName(), image.getAbsolutePath());
+
+        return image;
+    }
+
+    private void doTakePictureAndSave() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            File takeFile = null;
+            try {
+                takeFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (takeFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "vcoolwind.com.compositivesample.fileprovider",
+                        takeFile);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE_SAVE);
+            }
+
+        }
+    }
+
+    private void galleryAddPic(String file) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(file);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
 }
